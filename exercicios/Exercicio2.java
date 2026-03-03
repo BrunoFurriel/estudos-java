74023206c56a011701e67d77dc580daeca393071
BrunoFurriel <furrielbruno@gmail.com>
2026-03-03 20:36:09 -0300

    Create Exercicio2.java
    soma de duas variaveis

---
+++ exercicios/Exercicio2.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 8;
+        int soma = a + b;
+        System.out.println(a + "+" + b + "=" + soma);
+    }
+}
