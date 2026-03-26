9c7a2b3325109fe958bea7db275709c8d541637d
BrunoFurriel <furrielbruno@gmail.com>
2026-03-26 18:01:04 -0300

    Delete exercicios/Exercicio2.2.java

--- exercicios/Exercicio2.2.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String [] args){
-        double precoProduto = 30.00;
-        int quantidade = 5;
-        double total = precoProduto * quantidade;
-        System.out.println ("O total é: " + total);
-    }
-}
